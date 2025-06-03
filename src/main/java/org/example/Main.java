2150b8a9523745869df309a0df99b6476067826a
desarrollo <candresosorio1000@gmail.com>
2025-06-03 09:50:11 -0500

    refactor= se creo variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Reservas app");
+        System.out.println("----------\n");
+
+        //entradas del algoritmo
+        Scanner entradaTeclado=new Scanner(System.in);
+        LocalDate   fechaReserva;
+        LocalDate   fechaMantenimiento;
+
+        String nombreUsuario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamento;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacio;
+        String utensiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte nuemroBloque;
+        byte cantidadAsistentes;
+        byte edadUsuari;
+        int valorReserva;
+
+        boolean esAdministrador=false ;
+        boolean confirmacionReserva=false;
+        boolean estaOcupado=false;
+        boolean estaEnMantenimiento=false;
+
+
+
+
     }
+
 }
